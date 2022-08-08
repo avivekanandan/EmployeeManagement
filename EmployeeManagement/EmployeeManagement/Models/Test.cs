62e53b2730bad7283d5fa5dc4dec173cc7631a57
Vivekanandan Anbanandam <Vivekanandan.Anbanandam@avenuinsights.com>
2022-08-08 22:18:18 +0530

    Employee MAnagement

---
+++ EmployeeManagement/EmployeeManagement/Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EmployeeManagement.Models
+{
+    public class Test
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+       
+        public string Description { get; set; }
+
+        [ForeignKey("Employee")]
+        public int EployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
